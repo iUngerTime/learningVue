5c1a805ef993190be3f19a53668b46d9738f4ec3
iUngerTime <brenton.s.unger@live.com>
2021-01-01 23:42:26 -0800

    setup of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ]
+    },
+    "parserOptions": {
+        "ecmaVersion": 2015
+    }
+};
